159a44dc3814ff949f34353b5a8bb619f4cb9a0d
Abhi-2007 <abhiveersingh2007@gmail.com>
2022-03-20 20:45:56 +0530

    New Features Added

--- new.py
+++ new.py
@@ -6,23 +6,17 @@ import webbrowser
 import calendar
 import time
 from datetime import date
-# sum sub mul div area folder remove paste copy all slope file
-# add functionality to enter https links also
-# make a functionality to make and store files and open up files in the device itself
-# this will be very cool
-# function to check if next year is leap year or not
-
-fun = {
+all_functions = {
     "sum": "Adds two numbers entered",
     "sub": "Subtracts the first number from the second number",
     "mul": "Multiplies 2 numbers entered",
     "div": "Divides the first value entered by the second value entered",
-    "area": "finds the area of rectangle or square(Same as `mul`)".capitalize(),
+    "area": "finds the area of rectangle or square".capitalize(),
     "folder": "Creates a folder in the directory specified",
-    "remove": "Removes a file/folder",
+    "remove": "Removes a folder",
     "paste": "Pastes the text copied",
     "copy": "Copies the text typed",
-    "all": "Displays all the commands that can be typed",
+    "all": "Displays all the commands that are there",
     "slope": "calculates the slope of 4 values entered".capitalize(),
     "file": "Makes a new file in the specified directory",
     "link": "Opens the entered link in your default browser",
@@ -37,12 +31,106 @@ fun = {
     "write": "Creates a file and writes it if the user wants to",
     "timer": "Timer for how many ever seconds the user chooses",
     "word": "Counts the amount of words typed",
-    "today": "Prints the date in the format: YYYY|MM|DD"
+    "today": "Prints the date in the format: YYYY|MM|DD",
+    "youtube": "Opens Youtube in Default Browser",
+    "spotify": "Opens Spotify online Player in default browser",
+    "medium": "Opens medium on default browser",
+    "history": "Prints history",
+    "help": "Can tell what each function is used for",
+    "instaccount": "takes you to the instagram account you type your default browser in (exact name to be typed)",
+    "math": "Displays all mathematical functions",
+    "physics": "Displays all Physics Functions",
+    "fun": "Displays all miscellaneous(or fun) functions",
+    "os": "Displays all functions related to your operating system(windows)",
+    "internet": "Displays all functions related to the internet",
+    "time": "Displays all time related functions",
     }
+
+maths = {
+    "sum": "Adds two numbers entered",
+    "sub": "Subtracts the first number from the second number",
+    "mul": "Multiplies 2 numbers entered",
+    "div": "Divides the first value entered by the second value entered",
+    "area": "finds the area of rectangle or square(Same as `mul`)".capitalize(),
+    "mathall": "Shows all mathematical functions"
+}
+physics = {
+    "slope": "calculates the slope of 4 values entered".capitalize(),
+    "den": "Finds the density in grams per metre cube",
+    "vel": "Finds the velocity in metre per second",
+    "acc": "Finds the acceleration from the given values",
+    "vol": "Finds the volume",
+    "force": "Finds the force from the given mass and acceleration",
+    "work": "Finds work done or energy",
+}
+fun = {
+    "all": "Displays all the commands that are there",
+    "game": "Starts a game that is a surprise for You !!",
+
+
+}
+oss = {
+    "folder": "Creates a folder in the directory specified",
+    "remove": "Removes a folder",
+    "file": "Makes a new file in the specified directory",
+    "write": "Creates a file and writes it if the user wants to",
+}
+internet = {
+    "youtube": "Opens Youtube in Default Browser",
+    "spotify": "Opens Spotify online Player in default browser",
+    "medium": "Opens medium on default browser",
+    "instaccount": "takes you to the instagram account you type your default browser in (exact name to be typed)",
+    "link": "Opens the entered link in your default browser",
+}
+times = {
+    "timer": "Timer for how many ever seconds the user chooses",
+    "today": "Prints the date in the format: YYYY|MM|DD",
+}
+everything = [maths, physics, fun, oss, internet, times]
+
+
+def favourites():
+    favourite = []
+
+
+def account():
+    insta_name = input("Type the name of the instagram account you want to go to (exact name): ")
+    webbrowser.open_new_tab("https://www.instagram.com/{0}".format(insta_name))
+
+
+def to_do():
+    name = input("Enter the name of your file (including extension): ")
+    directory = input("Enter the directory where you want to store your file\n1.Documents\n2.Downloads\n3.Desktop\n: ")
+    path = f"C:/Users/abhiv/{directory}/{name}"
+    file = open(path, 'a')
+    num = int(input("How things do you want to put on this to-do list ?(number): "))
+    n = 0
+    while n != num:
+        n += 1
+        la = input("Type Here: ")
+        file.write(la + "\n")
+    file.close()
+
+
+def assist():
+    print("Type the keyword which you want help with: ")
+    for x, y in enumerate(all_functions.keys()):
+        print(x + 1, y)
+    key = input("Type Here: ")
+    if key in all_functions.keys():
+        print(all_functions[key])
+
+
+def medium():
+    webbrowser.open_new_tab("https://www.medium.com")
+
+
+def youtube():
+    webbrowser.open_new_tab("https://www.youtube.com")
 
 
 def spotify():
-    webbrowser.open("open.spotify.com")
+    webbrowser.open_new_tab("open.spotify.com")
 
 
 def today():
@@ -85,8 +173,8 @@ def link():
 
 
 def func():
-    for a, b in enumerate(fun):
-        print(a, b)
+    for a, b in (all_functions.items()):
+        print(a, ": ", b)
 
 
 def game():
@@ -96,7 +184,11 @@ def game():
     enter = int(input("Guess the number: "))
     while enter != me:
         enter = int(input("Guess the number: "))
-        if enter == me:
+        if enter > me:
+            print("Guess Lower")
+        elif enter < me:
+            print("Guess Higher")
+        elif enter == me:
             print("Congratulations! You got the right number")
 
 
@@ -107,19 +199,28 @@ def slope():
     x1 = float(input("Enter x1: "))
     one = y2 - y1
     two = x2 - x1
-    print(one/two)
+    print(one / two)
+
+
+def paste():
+    print(pyperclip.paste)
+
+
+def copy():
+    cop = pyperclip.copy(input("Type here: "))
+    print(f"{cop} copied to os clipboard!")
 
 
 def den():
     mass = float(input("Type mas in grams: "))
     volu = float(input("Type volume in metre cube: "))
-    print(f"Density is {mass/volu} ")
+    print(f"Density is {mass / volu} ")
 
 
-def vel():
+def speed():
     dis = float(input("Type the distance in metres: "))
     tim = float(input("Type time in second: "))
-    print("Velocity is {} m per second".format(dis/tim))
+    print("S    peed is {} m per second".format(dis / tim))
 
 
 def acc():
@@ -153,30 +254,30 @@ def mom():
 def torque():
     fo = float(input("Type force in N: "))
     dista = float(input("Type distance in metre: "))
-    print(f"Torque or moment of force is equal to {fo *dista} N m")
+    print(f"Torque or moment of force is equal to {fo * dista} N m")
 
 
 def power():
     wor = float(input("Type work in Joule: "))
     tim = float(input("enter time in seconds: "))
-    print(f"Power is equal to {wor/tim} J per second or Watt")
+    print(f"Power is equal to {wor / tim} J per second or Watt")
 
 
 def pressure():
     fo = float(input("Type force in N: "))
     are = float(input("Type area in metre square: "))
-    print(f"The pressure is {fo/are} N per metre square")
+    print(f"The pressure is {fo / are} N per metre square")
 
 
 def frequency():
     period = float(input("Type time period in seconds"))
-    print(f"Frequency is equal to {1/period} Hertz or second raised to -1")
+    print(f"Frequency is equal to {1 / period} Hertz or second raised to -1")
 
 
 def charge():
     current = float(input("Type current in ampere: "))
     time = float(input("Type time in second: "))
-    print(f"Electric charge is equal to {current*time} A s or C")
+    print(f"Electric charge is equal to {current * time} A s or C")
 
 
 def area():
@@ -188,13 +289,17 @@ def area():
     elif shape == "square".casefold():
         la = float(input("Type side of square"))
         print(la * la)
+    elif shape == "triangle".casefold():
+        base = float(input("Enter length of base in centimetres: "))
+        height = float(input("Enter the length of height in centimetres: "))
+        print("The area of this triangle is ", (base * height) / 2)
 
 
 def div():
     print("You have chosen division")
     two = float(input("Type the second number: "))
     one = float(input("Type the first number: "))
-    res = two/one
+    res = two / one
     if res == 0:
         print("Not defined")
     else:
@@ -215,9 +320,6 @@ def sub():
     print(c - d)
 
 
-print("Welcome")
-
-
 def add():
     print("You have chosen addition")
     a = float(input("Type the first number: "))
@@ -225,11 +327,29 @@ def add():
     print(a + b)
 
 
+def remove():
+    print("The only ones allowed are:\n1.Documents\n2.Downloads\n3.Desktop\nIn which directory is Your file in ?")
+    pla_2 = input("Type here: ")
+    fold_2 = input("What is the name of Your file ?")
+    os.rmdir(f"C:/Users/abhiv/{pla_2}/{fold_2}")
+    print(f"File: \"{fold_2}\" removed")
+
+
+def folder():
+    print("The only ones allowed are:\n1.Documents\n2.Downloads\n3.Desktop\nIn which directory is Your file in ?")
+    fil = input("Type Here: ")
+    place = input("What would You like to name Your folder?: ")
+    os.mkdir(f"C:/Users/abhiv/{fil}/{place}")
+
+
+bash = []
+
+print("Welcome")
+
 entry = input("PRESS ENTER")
 while entry != "exit":
     entry = input(">")
-    all_func = ("sum", "subtract", "multiplication", "division", "area", "paste", "copy", "all", "folder",
-                "remove")
+    bash.append(entry)
     if entry == "exit".casefold():
         break
     elif entry == "sum".casefold():
@@ -238,62 +358,39 @@ while entry != "exit":
         # print("Addition")
     elif entry == "sub".casefold():
         sub()
-        # all_func.append("subtract")
-        # print("Subtraction")
     elif entry == "mul".casefold():
-        # print("Multiplication")
         mul()
-        # all_func.append("multiplication")
     elif entry == "div".casefold():
-        # print("Division")
         div()
-        # all_func.append("division".title())
     elif entry == "area".casefold():
         area()
     elif entry == "":
         pass
     elif entry == "folder".casefold():
-        print("The only ones allowed are:\n1.Documents\n2.Downloads\n3.Desktop")
-        pla = input("Where do you want to store Your files ?")
-        fold = input("what is the name of Your file ?")
-        os.mkdir(f"C:/Users/abhiv/{pla}/{fold}")
+        folder()
     elif entry == "remove".casefold():
-        print("The only ones allowed are:\n1.Documents\n2.Downloads\n3.Desktop\nIn which directory is Your file in ?")
-        pla_2 = input("Type here: ")
-        fold_2 = input("What is the name of Your file ?")
-        os.rmdir(f"C:/Users/abhiv/{pla_2}/{fold_2}")
-        print(f"File: \"{fold_2}\" removed")
+        remove()
     elif entry == "paste":
         print(pyperclip.paste())
     elif entry == "copy":
-        cop = pyperclip.copy(input("Type here: "))
-        print(f"{cop} copied to os clipboard!")
-    # elif entry == f"help --{entry}":
-    #     if entry in
-    # elif entry == "list":
-    #     lists()
+        copy()
     elif entry == "all".casefold():
-        for i, j in enumerate(fun):
-            print(i+1, j, fun[j], sep=". ")
+        for i, j in enumerate(all_functions):
+            print(i + 1, j, all_functions[j], sep=". ")
     elif entry == "all".casefold():
-        # print("The following are the functions You can perform")
-        # print(all_func)
         func()
     elif entry == "slope".casefold():
         slope()
     elif entry == "folder".casefold():
-        print("The only ones allowed are:\n1.Documents\n2.Downloads\n3.Desktop\nIn which directory is Your file in ?")
-        fil = input("Type Here: ")
-        place = input("What would You like to name Your folder?: ")
-        os.mkdir(f"C:/Users/abhiv/{fil}/{place}")
+        folder()
     elif entry == "game".casefold():
         game()
     elif entry == "den".casefold():
         den()
-    elif entry == "vel".casefold():
-        vel()
+    elif entry == "vol".casefold():
+        vol()
     elif entry == "speed":
-        vel()
+        speed()
     elif entry == "force".casefold():
         force()
     elif entry == "work".casefold():
@@ -316,16 +413,44 @@ while entry != "exit":
         write()
     elif entry == "leap":
         leap()
-    elif entry == "write":
-        write()
     elif entry == "timer":
         timer()
     elif entry == "word":
         word()
     elif entry == "today":
         today()
+    elif entry == "acc":
+        acc()
     elif entry == 'spotify':
         spotify()
+    elif entry == "youtube":
+        youtube()
+    elif entry == "history":
+        for i in bash:
+            print(i)
+    elif entry == "medium":
+        medium()
+    elif entry == "help":
+        assist()
+    elif entry == "to do list":
+        to_do()
+    elif entry == "instaccount":
+        account()
+    elif entry == "math":
+        for z in maths.keys():
+            print(z, maths[z], sep=": ")
+    elif entry == "physics":
+        for x in physics.keys():
+            print(x, physics[x], sep=": ")
+    elif entry == "os":
+        for a in oss.keys():
+            print(a, oss[a], sep=": ")
+    elif entry == "internet":
+        for b in internet.keys():
+            print(b, internet[b], sep=": ")
+    elif entry == "time":
+        for t in times.keys():
+            print(t, times[t], sep=": ")
     else:
         print(f"could not understand what {entry} meant.Try something from the list below")
         func()
